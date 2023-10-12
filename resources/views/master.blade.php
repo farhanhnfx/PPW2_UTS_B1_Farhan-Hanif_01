0a88248efe1d5065b77a9b2cbd47310e47b16940
farhanhnfx <farhanhanif@mail.ugm.ac.id>
2023-10-12 13:47:41 +0700

    revisi header & master layout

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -9,9 +9,12 @@
 </head>
 <body>
     @include('pemain.header')
-    <h3 class="text-center">@yield('title')</h3>
+    <div class="container">
+        <h3 class="text-center">@yield('title')</h3>
+    </div>
     <div class="container">
         @yield('content')
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
--- resources/views/pemain/header.blade.php
+++ resources/views/pemain/header.blade.php
@@ -1,5 +1,5 @@
 <header>
-  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
     <div class="container-fluid">
       <a class="navbar-brand" href="#">UTS PPW2</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
